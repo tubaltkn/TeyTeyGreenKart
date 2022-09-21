242ae6b51ec372f149847a58b4c259183d40c23b
Alt_Scape <tubaltkn@gmail.com>
2022-09-21 14:30:01 +0100

    faker ile search boxa gonderme added

--- src/test/java/com/greenKart/stepDefs/searchFunctionStepDef.java
+++ src/test/java/com/greenKart/stepDefs/searchFunctionStepDef.java
@@ -76,3 +76,45 @@ public class searchFunctionStepDef extends BasePage {
 //    public void userWillBeAbleToTypeInTheSearchBox() {
 //    }
 }
+
+//--------------------Faker ile search boxa gonderme-----------------------------
+
+
+//    com.greenKart.pages.ProductPage ProductPage = new ProductPage();
+//    Faker faker = new Faker();
+//
+//
+//
+//    @When("User clicks on search box")
+//    public void userClicksOnSearchBox() {
+//
+//        ProductPage.searchBox.click();
+//    }
+//
+//    @When("User enters random characters in the search box.")
+//    public void userEntersRandomCharactersInTheSearchBox() {
+//        ProductPage.searchBoxBefore= ProductPage.searchBox.getText();
+//
+//        ProductPage.searchBox.sendKeys(faker.letterify("??"));
+//
+//    }
+//
+//    @Then("User will be able to type in the search box.")
+//    public void userWillBeAbleToTypeInTheSearchBox() {
+//
+//        ProductPage.searchBoxAfter= ProductPage.searchBox.getText();
+//
+//        Assert.assertNotSame(ProductPage.searchBoxBefore, ProductPage.searchBoxAfter);
+//
+//        System.out.println("ProductPage.searchBoxBefore = " + ProductPage.searchBoxBefore);
+//
+//
+//
+//    }
+//
+//    @When("User enters a random product name")
+//    public void userEntersARandomProductName() {
+//
+//        ProductPage.getRandomProduct();
+//    }
+//}
